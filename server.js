1d9b7f3fe8c93f4a5b3c266cfc18d21a9e1486e6
Nikesh Uprety <upretynikesh123@gmail.com>
2023-02-24 00:35:55 +0545

    Moving- server,js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from  'path';
 import admin from 'firebase-admin';
 import express from 'express';
 import 'dotenv/config'
-import { db, connectToDB } from './db.js';
+import { db, connectToDB } from './src/db.js';
 
 import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
